b91e4c4d89ed24a5c5937390804d6dfc87d5864c
12663611009 <pisos.louiejay53@gmail.com>
2025-02-15 12:55:54 +0800

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,52 @@
+#My first python program
+#remove the (#) the run the other codes
+print("Hello, python")
+#The print is to print the output of your code
+#code will not execute if the qoutation mark is remove because the code i input is a string
+
+#name = "Alice"  #string
+#age = "twenty"     #integer
+#height = 5.7  #float
+#is_student = True  #boolean
+
+
+#print("name", name, "type", type(name))
+#print("age", age, "type", type(age))
+#print("height", height, "type", type(height))
+#print("is_student", is_student, "type", type(is_student))
+#type function is to see what data type use in your variable
+#if you change the age to "twenty" it will stil run but it will change into a string because of the qoutation mark
+
+
+
+#num1 = 222
+#num2 = 89
+#performing operation
+#sum_result = num1 + num2 #sum
+#diff_result = num1 - num2 #minus
+#prod_result = num1 * num2 #multiply
+#qoutient_result = num1 / num2 #division (float)
+#int_qoutient_result = num1 // num2 # int_division
+#modulos_result = num1 % num2 #modulos
+#exp_result = num1 ** num2 #exponintiution
+
+#displaying results
+#print("diff", diff_result)
+#print("prod", prod_result)
+#print("qoutient", qoutient_result)
+#print("int_qoutient", int_qoutient_result)
+#print("modulos", modulos_result)
+#print("exp", exp_result)
+# the difference between / and // is that // removes the decimal poitn when dividing
+# after modifying num1 and num2 code still runs with whatever number you choose
+
+
+
+
+#length = float(input("Enter length:"))
+#width = float(input("Enter width:"))
+#area of rectangle
+#area = length + length + width + width #Modified to calculate the parameter of rectangle instead of area
+
+#print("The area of Rectangle is", area) 
+#if you put string instead of a number code will error because the data type is float which is a integer with a decimal point
